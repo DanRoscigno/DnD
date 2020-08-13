669dd93c058e2c263e008bb4d1a6aa76efe7ad59
Dan Roscigno <dan@roscigno.com>
2020-08-12 20:30:13 -0400

    add notes about ATK and Damage rolls

--- characters/darvas-level-3.py
+++ characters/darvas-level-3.py
@@ -1,10 +1,8 @@
 """This file describes the heroic adventurer Darvas.
 
-It's used primarily for saving characters from create-character,
-where there will be many missing sections.
-
-Modify this file as you level up and then re-generate the character
-sheet by running ``makesheets -F filename.py`` from the command line.
+Modify this file as you level up and then re-generate 
+the character sheet by running ``makesheets -F filename.py`` 
+from the command line.
 
 """
 
@@ -34,16 +32,15 @@ wisdom = 12
 charisma = 8
 
 # Select what skills you're proficient with
-# ex: skill_proficiencies = ('athletics', 'acrobatics', 'arcana')
 skill_proficiencies = ('acrobatics', 'investigation', 'perception', 'deception', 'sleight of hand', 'stealth')
 
-# Any skills you have "expertise" (Bard/Rogue) in
 skill_expertise = ('sleight of hand', 'stealth')
 
-# Named features / feats that aren't part of your classes, race, or background.
-# Also include Eldritch Invocations and features you make multiple selection of
-# (like Maneuvers for Fighter, Metamagic for Sorcerors, Trick Shots for
-# Gunslinger, etc.)
+# Named features / feats that aren't part of your classes, 
+# race, or background.
+# Also include Eldritch Invocations and features you make 
+# multiple selection of (like Maneuvers for Fighter, 
+# Metamagic for Sorcerors, Trick Shots for Gunslinger, etc.)
 # Example:
 # features = ('Tavern Brawler',) # take the optional Feat from PHB
 features = ()
@@ -54,29 +51,27 @@ features = ()
 feature_choices = ()
 
 # Weapons/other proficiencies not given by class/race/background
-weapon_proficiencies = ('shortsword', 'rapier')  # ex: ('shortsword', 'quarterstaff')
-_proficiencies_text = ("thieves' tools",)  # ex: ("thieves' tools",)
+weapon_proficiencies = ('dagger', 'shortbow', 'rapier')
+_proficiencies_text = ("thieves' tools",)
 
 # Proficiencies and languages
 languages = """Common, Halfling"""
 
 # Inventory
-# TODO: Get yourself some money
 cp = 0
 sp = 0
 ep = 0
-# rolled 11 for starting gold
+# rolled 4+3+3+1 for starting gold
 # 15 for being a criminal
 # 4 treasure points from Adventurer's League (1 point = 50gp) 
 # - 10 to buy shield
 gp = (4 + 3 + 3 + 1) * 10 + 15 + (4 * 50) - 10
 pp = 0
 
-# TODO: Put your equipped weapons and armor here
-weapons = ('rapier', 'dagger', 'shortbow')  # Example: ('shortsword', 'longsword')
+weapons = ('rapier', 'dagger', 'shortbow')
 magic_items = ()  # Example: ('ring of protection',)
-armor = "leather armor"  # Eg "leather armor"
-shield = "shield"  # Eg "shield"
+armor = "leather armor"
+shield = "shield"
 
 # See equipment packs on page 151 of PHB
 # Page 129 of PHB: Criminal starting equip: 
@@ -84,8 +79,11 @@ shield = "shield"  # Eg "shield"
 # including a hood, and a belt pouch containing 15 gp
 equipment = """TODO: list the equipment and magic items your character carries"""
 
-attacks_and_spellcasting = """TODO: Describe how your character usually attacks
-or uses spells."""
+attacks_and_spellcasting = """Notes: ATK Bonus is my
+                              proficiency bonus + Dex;
+                              Damage is roll + Dex on
+                              finesse weapons.  Why is
+                              shortbow also +3?"""
 
 # List of known spells
 # Example: spells_prepared = ('magic missile', 'mage armor')
@@ -102,8 +100,9 @@ wild_shapes = ()  # Ex: ('ape', 'wolf', 'ankylosaurus')
 
 # Backstory
 # Describe your backstory here
-personality_traits = """TODO: How does your character behave? See the PHB for
-examples of all the sections below"""
+personality_traits = """TODO: How does your character
+                        behave? See the PHB for
+                        examples of all the sections below"""
 
 ideals = """TODO: What does your character believe in?"""
 
@@ -113,5 +112,6 @@ and other commitments or ongoing quests they have."""
 flaws = """TODO: Describe your characters interesting flaws.
 """
 
-features_and_traits = """TODO: Describe other features and abilities your
-character has."""
+features_and_traits = """TODO: Describe other features
+                         and abilities your
+                         character has."""
