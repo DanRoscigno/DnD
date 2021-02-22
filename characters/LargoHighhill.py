5c3351815b4bb7427dbf04ec9c1029f544194a19
DanRoscigno <dan@roscigno.com>
2021-02-22 03:15:56 +0000

    init Largo

---
+++ characters/LargoHighhill.py
@@ -0,0 +1,109 @@
+"""This file describes the heroic adventurer Charles.
+
+It's used primarily for saving characters from create-character,
+where there will be many missing sections.
+
+Modify this file as you level up and then re-generate the character
+sheet by running ``makesheets`` from the command line.
+
+"""
+
+dungeonsheets_version = "0.10.2"
+
+name = "Charles"
+player_name = "Dan R"
+
+# Be sure to list Primary class first
+classes = ['Sorceror']  # ex: ['Wizard'] or ['Rogue', 'Fighter']
+levels = [2]  # ex: [10] or [3, 2]
+subclasses = ["Draconic Bloodline"]  # ex: ['Necromacy'] or ['Thief', None]
+background = "Guild Artisan"
+race = "Lightfoot Halfling"
+alignment = "Neutral good"
+
+xp = 0
+hp_max = 9
+inspiration = False  # boolean inspiration value
+
+# Ability Scores
+strength = 13
+dexterity = 14
+constitution = 15
+intelligence = 11
+wisdom = 11
+charisma = 16
+
+# Select what skills you're proficient with
+# ex: skill_proficiencies = ('athletics', 'acrobatics', 'arcana')
+skill_proficiencies = ('deception', 'intimidation', 'insight', 'persuasion')
+
+# Any skills you have "expertise" (Bard/Rogue) in
+skill_expertise = ()
+
+# Named features / feats that aren't part of your classes, race, or background.
+# Also include Eldritch Invocations and features you make multiple selection of
+# (like Maneuvers for Fighter, Metamagic for Sorcerors, Trick Shots for
+# Gunslinger, etc.)
+# Example:
+# features = ('Tavern Brawler',) # take the optional Feat from PHB
+features = ()
+
+# If selecting among multiple feature options: ex Fighting Style
+# Example (Fighting Style):
+# feature_choices = ('Archery',)
+feature_choices = ()
+
+# Weapons/other proficiencies not given by class/race/background
+weapon_proficiencies = ()  # ex: ('shortsword', 'quarterstaff')
+_proficiencies_text = ()  # ex: ("thieves' tools",)
+
+# Proficiencies and languages
+languages = """[choose one], [choose one], Common, Halfling"""
+
+# Inventory
+# TODO: Get yourself some money
+cp = 0
+sp = 0
+ep = 0
+gp = 0
+pp = 0
+
+# TODO: Put your equipped weapons and armor here
+weapons = ["Dagger", "Light crossbow", "Quarterstaff"]  # Example: ('shortsword', 'longsword')
+magic_items = ()  # Example: ('ring of protection',)
+armor = "None"  # Eg "leather armor"
+shield = "None"  # Eg "shield"
+
+equipment = """small bottle w/ dead sprite"""
+
+attacks_and_spellcasting = """TODO: Describe how your character usually attacks
+or uses spells."""
+
+# List of known spells
+# Example: spells_prepared = ('magic missile', 'mage armor')
+spells_prepared = ('ray of frost', 'message', 'dancing lights', 'mage hand', 'detect magic', 'sleep', 'charm person')  # Todo: Learn some spells
+
+# Which spells have not been prepared
+__spells_unprepared = ()
+
+# all spells known
+spells = spells_prepared + __spells_unprepared
+
+# Wild shapes for Druid
+wild_shapes = ()  # Ex: ('ape', 'wolf', 'ankylosaurus')
+
+# Infusions for Artificer
+infusions = () # Ex: ('repeating shot', 'replicate magic item')
+
+# Backstory
+# Describe your backstory here
+personality_traits = """TODO: Describe how your character behaves, interacts with others"""
+
+ideals = """TODO: Describe what values your character believes in."""
+
+bonds = """TODO: Describe your character's commitments or ongoing quests."""
+
+flaws = """TODO: Describe your character's interesting flaws."""
+
+features_and_traits = """TODO: Describe other features and abilities your
+character has."""
